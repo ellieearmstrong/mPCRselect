0ce8f26bda9c493df987e03e7e0e918c2c3e07b8
Michael G. Campana <campanam@si.edu>
2024-05-05 15:03:28 -0400

    site list compatible

--- bin/get_best_snps.rb
+++ bin/get_best_snps.rb
@@ -5,15 +5,31 @@
 
 require 'zlib'
 
+#-----------------------------------------------------------------------------------------------
+# From BaitsTools 1.6.8: Campana 2018
+def gz_file_open(file) # Determine whether input file is gzipped or not and set method to open it
+	if file[-3..-1] == ".gz"
+		yield Zlib::GzipReader.open(file)
+	else
+		yield File.open(file)
+	end
+end
+#-----------------------------------------------------------------------------------------------
+
 @maxSNPs = ARGV[0].to_i
 @snps = {} # Count of SNP occurrences keyed by chr and pos
 
 for infile in ARGV[1..-1]
-	Zlib::GzipReader.open(infile) do |f1|
+	gz_file_open(infile) do |f1|
 		while line = f1.gets
 			if line[0].chr != '#'
 				line_arr = line.split
-				site = line_arr[0..1].join("\t")
+				if line_arr.size == 1 # Determine if input is VCF or site list
+					site = line_arr[0].split("_")[0].gsub("\"","").gsub(":","\t") # Use PLINK snp ID and remove SNP call for site lists
+				else
+					site = line_arr[2].split("_")[0].gsub(":","\t") # Use PLINK snp ID and remove SNP call for VCFs
+				end
+				puts site
 				if @snps.keys.include?(site)
 					@snps[site]+=1
 				else
