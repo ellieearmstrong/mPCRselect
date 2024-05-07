deb50b22b73fc3c1ba788f40dd78cea5a9313e42
Michael G. Campana <campanam@si.edu>
2024-05-06 23:14:04 -0400

    Update plotPCA.R

--- bin/plotPCA.R
+++ bin/plotPCA.R
@@ -2,6 +2,8 @@
 
 # Simple plot of PLINK2 PC1 versus 2
 
+library(ggplot2)
+
 # Set up arguments
 pca_args = commandArgs(trailingOnly=TRUE) # Get input arguments
 pca <- read.table(pca_args) # PLINK2 eigenvec file
@@ -10,9 +12,10 @@ total_variance <- sum(read.table(gsub(".eigenvec",".rel.diag", pca_args)))
 pc1_percent <- round((eigenval[1,]/total_variance)*100, digits = 2)
 pc2_percent <- round((eigenval[2,]/total_variance)*100, digits = 2)
 
-#pdf(gsub(".eigenvec", ".pca.pdf", pca_args), width = 10, height = 10)
-#	pc1lab = paste("PC1:", pc1_percent, "%")
-#	pc2lab = paste("PC2:", pc2_percent, "%")
-#	plot(pca$V2, pca$V3, xlab = pc1lab, ylab = pc2lab)
-#dev.off()	
+gsub(".eigenvec", ".pca.pdf", pca_args)
+png(gsub(".eigenvec", ".pca.png", pca_args), width = 10, height = 10)
+	pc1lab = paste("PC1: ", pc1_percent, "%", sep = "")
+	pc2lab = paste("PC2: ", pc2_percent, "%", sep = "")
+	plot(pca$V2, pca$V3, xlab = pc1lab, ylab = pc2lab)
+dev.off()	
 
